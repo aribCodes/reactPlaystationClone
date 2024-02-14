b21be0ea6c0270a764a36f6431ac484c6a06fdc2
syediqran2005 <syediqran2004@gmail.com>
2024-02-14 18:28:14 +0500

    iqran codes

--- SonyGameWebClone/src/App.jsx
+++ SonyGameWebClone/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { DatePicker } from 'antd';
-
+import GameSlider from './components/sliders/gamesSlider/GameSlider';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        
+        <GameSlider />
       </div>
     </>
   )
